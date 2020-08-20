467385790e46d091470a1e2f84eb4867b4cc056c
Travis Byrum <travis.tbyrum@gmail.com>
2020-08-20 13:51:29 -0400

    Remove underscore case files

--- R/partial_correlation_influence.R
+++
@@ -1,6 +0,0 @@
-#Reconstruction of graphs using the partial correlation influence, as defined in:
- # Kenett, D. Y. et al. Dominating clasp of the financial sector revealed by
-#partial correlation analysis of the stock market. PLoS ONE 5, e15032 (2010).
-#The index variable option as in:
- # Kenett, D. Y., Huang, X., Vodenska, I., Havlin, S. & Stanley, H. E. Partial correlation
-#analysis: applications for financial markets. Quantitative Finance 15, 569–578 (2015).
