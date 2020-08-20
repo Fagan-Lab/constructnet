24230e45a58df92e8633f75eab1ab297926133c1
mtchuindjo <mtchuindjo@gmail.com>
2020-08-20 12:09:26 -0400

    documentation refactor

---
+++ R/Random.R
@@ -0,0 +1,12 @@
+#Reconstruct a network from a random matrix, not taking time series into account
+#@export
+inputThreshold <- threshold(inputArray, min = -Inf, max = Inf)
+construct_random <- function(inputArray, inputThreshold,...) UseMethod("construct_random")
+#@export
+construct_random.igraph <- function(inputArray, inputThreshold,...)
+rowsinMatrix <- nrow(inputArray)
+colsinMatrix <- ncol(inputArray)
+randomMatrices <-randn(rowsinMatrix, rowsinMatrix)
+A<- threshold(randomMatrices,inputThreshold,...)
+G<-graph_from_literal(A, simplify=FALSE)#allow for nodes
+
