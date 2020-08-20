641f4f1e492906466174971665d28d201cdec8e2
Travis Byrum <travis.tbyrum@gmail.com>
2020-08-20 13:54:47 -0400

    Make package build

--- R/correlation-spanning-tree.R
+++ R/correlation-spanning-tree.R
@@ -1,24 +1,25 @@
-#Creates a minimum spanning tree connecting the sensors.
-#'@export
+#' Creates a minimum spanning tree connecting the sensors.
+#' @export
 #distance <-distances(inverse.sqrt)
-construct_correlation_spanning <-function(inputArray, distance, ...)UseMethod("construct_correlation_spanning")
+construct_correlation_spanning <- function(inputArray, distance, ...) UseMethod("construct_correlation_spanning")
 
-#'@export
+#' @export
 construct_correlation_spanning.igraph <-function(inputArray, distance, ...){
-  rowsinMatrix <- nrow(inputArray)
-  colsinMatrix <- ncol(inputArray)
-  correlation_matrix <- cor(inputArray, method = "pearson") #create correlation matrix
-
-  if(distance == inverse.sqrt){ #Distance matrix
-    D <- sqrt(2*(1-correlation_matrix))
-  }else{
-    D <- 1-square(correlation_matrix)
-  }
-  MST <- mst(D)
-
-  asSparse <- sparseMatrix(MST)#convert to sparse matrix
-  asAdjacency <-as_adjacency_matrix(asSparse)#convert to adjacency matrix
-
-  G<-graph_from_adjacency_matrix(asAdjacency)#generate graph
+  # rowsinMatrix <- nrow(inputArray)
+  # colsinMatrix <- ncol(inputArray)
+  # correlation_matrix <- cor(inputArray, method = "pearson") #create correlation matrix
+  #
+  # if (distance == inverse.sqrt){ #Distance matrix
+  #   D <- sqrt(2*(1-correlation_matrix))
+  # } else{
+  #   D <- 1 - square(correlation_matrix)
+  # }
+  #
+  # MST <- mst(D)
+  #
+  # asSparse <- sparseMatrix(MST) #convert to sparse matrix
+  # asAdjacency <- as_adjacency_matrix(asSparse)#convert to adjacency matrix
+  #
+  # G <- graph_from_adjacency_matrix(asAdjacency)#generate graph
 }
 
