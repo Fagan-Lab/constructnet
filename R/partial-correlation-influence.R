47fe5ffc579badd7c9925262ecaf643b69d54d59
daniellejiaxuan <daniellejiaxuan@gmail.com>
2021-03-15 14:34:11 -0400

    naive_transfer_entropy R file

--- R/partial-correlation-influence.R
+++ R/partial-correlation-influence.R
@@ -1,6 +1,24 @@
-#' Reconstruction of graphs using the partial correlation influence, as defined in:
-#' Kenett, D. Y. et al. Dominating clasp of the financial sector revealed by
-#' partial correlation analysis of the stock market. PLoS ONE 5, e15032 (2010).
-#' The index variable option as in:
-#' Kenett, D. Y., Huang, X., Vodenska, I., Havlin, S. & Stanley, H. E. Partial correlation
-#' analysis: applications for financial markets. Quantitative Finance 15, 569–578 (2015).
+#' Reconstruction of graphs using the partial correlation influence.
+#'
+#'
+#' @param TS Input matrix, X * L matrix consisting of L observations from N seneors.
+#' @param index An index variable or set of index variables, which are assumed
+#' to be confounders of all other variables.
+#' @param threshold_type Which thresholding function to use on the matrix of weights
+#' @param ... arguments
+#'
+#' @return A reconstructed graph with N nodes
+#' @export
+partial_correlation_influence <- function(TS, index=None, threshold_type='range', ...) {
+  data <- t(TS)
+  N = nrow(data)
+
+  mask = matrix(1, N)
+  if (!is.null(index)) {
+    mask[index] <- FALSE
+  }
+
+  p_corr = matrix(NA, N, N)
+  # FIX #
+  p_corr = partial_corr(data[, mask], )
+}
